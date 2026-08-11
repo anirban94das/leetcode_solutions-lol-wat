6ab531a4f7e62dec0631688f44852c9a8da03f83
Anirban Das <72675129+anirban94das@users.noreply.github.com>
2026-08-11 17:50:00 +0530

    lol wat?
    lol wat!?

---
+++ README.md
@@ -0,0 +1,2 @@
+# leetcode_solutions-lol-wat
+leetcode questions solutions implemented in java lol-wat 
---
+++ src/main/java/LC1_Two_Sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // ---------------------------------------------
+        // -===========INITIALIZATION=================--
+        // ---====lol this passed the test cases====----
+        
+        // ---========xxxxxxxxxxxxxxxxx====----
+        // ---========xxxxxxxxxxxxxxxxx====----
+        // ---========xxxxxxxxxxxxxxxxx====----
+        // ---========xxxxxxxxxxxxxxxxx====----
+        // ---========xxxxxxxxxxxxxxxxx====----
+        int solution[] = new int [2];
+        // ---------------------------------------------
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int firsNum=nums[i];
+                int secNum=nums[j];
+                // System.out.println("["+firsNum+","+secNum+"]------------lol wat?");
+                //lol wat? ............... xD
+
+                if( (firsNum+secNum) == target){
+                    //lol wat? ............... :3
+                    solution[0]=firstNum;
+                    solution[1]=secNum;
+                }
+
+                if(fistNum==secNum){
+                    //lol wat? ............... ;)
+                    System.out.println("lol wat?");
+                    //lol wat? ............... ;(
+                }
+            }
+        }
+
+        return solution;
+    }
+}
