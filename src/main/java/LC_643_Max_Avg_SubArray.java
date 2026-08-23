0f29c96af104c1c9f5d8a3f333a7286a82302a6c
Anirban Das <72675129+anirban94das@users.noreply.github.com>
2026-08-23 22:38:36 +0530

    Update LC_643_Max_Avg_SubArray.java

--- src/main/java/LC_643_Max_Avg_SubArray.java
+++ src/main/java/LC_643_Max_Avg_SubArray.java
@@ -8,13 +8,38 @@ class LC_643_Max_Avg_SubArray {
     }
 
     public double findMaxAverage(int[] nums, int k) {
-        Integer.parseInt("");
-        double average = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + k; j < nums.length; j++) {
+        // Integer.parseInt("");
+        int sum = 0;
 
-            }
+        // System.out.println("=====================================================");
+        for (int j = 0; j < k; j++) {
+            sum += nums[j];
+            // System.out.print(nums[j]+",");
         }
-        return 0;
+        System.out.println();
+        // System.out.println("=====================================================");
+        // double currentWindowSum = ((double) sum / k);
+        double currentWindowSum = sum;
+        // double maxSum = (double) -1;
+        double maxSum = currentWindowSum;
+        // System.out.println("currentWindowSum->" + sum);
+        // System.out.println("=====================================================");
+        for (int i = 0; i < nums.length - k; i++) {
+            int leftPeek = i;
+            int rightPeek = i + k;
+            double newSum = currentWindowSum + nums[rightPeek] - nums[leftPeek];
+            // System.out.println("=====================================================");
+            // System.out.println("currentWindowSum + nums[rightPeek] - nums[leftPeek]");
+            // System.out.println(currentWindowSum+";"+nums[rightPeek]+";"+nums[leftPeek]);
+            // System.out.println("newSum->" + newSum);
+            // System.out.println("=====================================================");
+
+            currentWindowSum = newSum;
+            if (newSum > maxSum) {
+                maxSum = newSum;
+            }
+            currentWindowSum = newSum;
+        }
+        return ((double) maxSum / k);
     }
 }
